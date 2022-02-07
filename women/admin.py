74656dca21287671ef2a2c4c6f5223ea0a87a91e
NKP144 <k.nikonov@big3.ru>
2022-02-07 19:32:43 +0300

    Work with Admin Panel

--- women/admin.py
+++ women/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import *
+
+
+class WomenAdmin(admin.ModelAdmin):
+    """Класс-редактор для задания параметров представления модели"""
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')  # Поля, которые выводятся в списке записей
+    list_display_links = ('id', 'title')                 # Поля, которые преобразуются в гиперссылки для правки записей
+    search_fields = ('title', 'content')                 # Поля для поиска
+    list_editable = ('is_published',)                    # Список редактируемых полей
+    list_filter = ('is_published', 'time_create')        # Поля для фильтрации
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Класс-редактор для задания параметров представления модели"""
+    list_display = ('id', 'name')  # Поля, которые выводятся в списке записей
+    list_display_links = ('id', 'name')                 # Поля, которые преобразуются в гиперссылки для правки записей
+    search_fields = ('name',)                 # Поля для фильтрации
+
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
+
 # Register your models here.
--- women/models.py
+++ women/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)  # blank - поле м.б. пустым
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")     # Хранит путь к файлу
-    time_create = models.DateTimeField(auto_now_add=True)   # Фиксирует текущее время в момент добавления записи и менятся не будет
-    time_update = models.DateTimeField(auto_now=True)   # Время будет меняться каждый раз, когда происходит обновление записи
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)  # Внешний ключ на таблицу Category
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")  # blank - поле м.б. пустым
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")     # Хранит путь к файлу
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")   # Фиксирует текущее время в момент добавления записи и менятся не будет
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")   # Время будет меняться каждый раз, когда происходит обновление записи
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категория")  # Внешний ключ на таблицу Category
 
     def __str__(self):
         return self.title
@@ -17,12 +17,22 @@ class Women(models.Model):
     def get_absolute_url(self):  # Это функция используется в шаблоне для возврата ссылки на элемент из БАЗЫ ДАННЫХ
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = "Известные женщины"
+        verbose_name_plural = "Известные женщины"
+        ordering = ['time_create', 'title']
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['id']
