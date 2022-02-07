eb47d21359fdb0c7a54bb6069e62036c6dda174f
NKP144 <k.nikonov@big3.ru>
2022-02-07 23:20:28 +0300

    lesson 11. Пользовательские тэги. Находятся в модуле tamlatetags, в файле women_tags. Теги могут иметь имена и параметры. Шаблоны для тэгов хранятся в папке с шаблонами. Сначала надо зарегистрировать и загрузить в шаблоне {% load women_tags %}

---
+++ women/templatetags/women_tags.py
@@ -0,0 +1,34 @@
+from django import template
+from django.http import Http404
+from women.models import *
+
+register = template.Library()
+
+
+@register.simple_tag(name='getcats')  # simple tag
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
+
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories(sort=None, cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+    return {"cats": cats, "cat_selected": cat_selected}
+
+
+@register.inclusion_tag('women/list_posts.html')
+def show_posts(cat_selected=0):
+    if not cat_selected:
+        posts = Women.objects.all()
+    else:
+        posts = Women.objects.filter(cat_id=cat_selected)
+        if len(posts) == 0:
+            raise Http404()
+
+    return {"posts": posts}
--- women/views.py
+++ women/views.py
@@ -10,13 +10,9 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 
 def index(request):  # HttpRequest
-    posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
-        'posts': posts,
         'menu': menu,
         'title': 'Главная страница',
-        'cats': cats,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=context)
@@ -57,23 +53,12 @@ def show_post(request, post_id):
 
 
 def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
-
-    if len(posts) == 0:
-        raise Http404()
-
     context = {
-        'posts': posts,
         'menu': menu,
         'title': 'Отображение по рубрикам',
-        'cats': cats,
         'cat_selected': cat_id,
     }
     return render(request, 'women/index.html', context=context)
-
-
-
 
 
 def pageNotFound(reques, exception):
